9f881e3ba8d96c5f243dcedf59ca8c684c99c2b5
Jake Provard <provardjake@gmail.com>
2023-09-04 18:31:31 -0600

    added js to delete note from database

--- server.js
+++ server.js
@@ -19,8 +19,6 @@ app.get('/', (req, res) =>
 
 app.get("/notes", (req, res) => {
     res.sendFile(path.join(__dirname, "/public/notes.html"));
-
-
 });
 
 app.get("/api/notes", (req, res) =>{
@@ -59,12 +57,34 @@ app.post("/api/notes", (req, res)=>{
 
             fs.writeFile("db/db.json", JSON.stringify(currentNotes, null, 4), (writeErr) => {
                 writeErr ? console.log(writeErr) : console.info("Added new note!");
+                res.sendFile(path.resolve('./db/db.json'));
             })
         }
     })
 });
 
+app.delete("/api/notes/:id", (req, res) =>{
+    const {id} = req.params;
 
+    fs.readFile("./db/db.json", "utf8", (err, data) =>{
+        if(err){
+            console.log(err);
+        }
+        else{
+            const currentNotes = JSON.parse(data);
+
+            const newNotes = currentNotes.filter((note) => {
+                 return note.id != id; 
+            });
+
+            fs.writeFile("db/db.json", JSON.stringify(newNotes, null, 4), (writeErr) => {
+                writeErr ? console.log(writeErr) : console.info("Removed note!");
+                res.sendFile(path.resolve('./db/db.json'));
+            })
+        }
+    })
+
+});
 
 app.listen(PORT, ()=>{
     console.log(`App listening at http://localhost:${PORT}`);
